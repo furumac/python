a640b54e8cd3fb32e5cc8610983465e95510049f
フルマック <furumac@furumakkunoMacBook-Air.local>
2020-03-22 19:47:17 +0900

    メソッドの抽出

--- insert_sql.py
+++ insert_sql.py
@@ -7,36 +7,46 @@ import errno
 import MySQLdb
 
 TABLE_NAME = 'test_table'
-TABLE_COLUMN_COUNT = 2
+TABLE_COLUMN_COUNT = 2  # INSERT時に指定するカラム数
 
 config = configparser.ConfigParser()
 config_path = '../ini/python_db_config.ini'
+input_csv_path = 'csv/db_insert.csv'
 
-def main():
 
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_path)
-
+def config_read():
     config.read(config_path, encoding='utf-8') 
 
+
+def connect_db():
     db = config['DEFAULT']['Db']
     user = config['DEFAULT']['User']
     passwd = config['DEFAULT']['Passwd']
 
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), config_path)
+
     connect = MySQLdb.connect(db=db, user=user, passwd=passwd)
     cursor = connect.cursor()
+    return connect, cursor
 
+def drop_table(cursor):
     cursor.execute(f"DROP TABLE IF EXISTS {TABLE_NAME}")
-
     print(f"TABLE [{TABLE_NAME}] をDROPしました。")
 
-    cursor.execute(f"create table {TABLE_NAME} (id int(8) PRIMARY KEY, url char(255))")
 
+def create_table(cursor):
+    cursor.execute(f"create table {TABLE_NAME} (id int(8) PRIMARY KEY, url char(255))")
     print(f"TABLE [{TABLE_NAME}] をCREATEしました。")
 
-    with open('csv/db_insert.csv') as csv:
+
+def insert_db_from_csv(cursor):
+    with open(input_csv_path) as csv:
         for i, line in enumerate(csv):
             list = line.strip().split(',')
+            
+            # カンマ区切りの数がINSERTでセットするカラム数に一致しているか
             if len(list) != TABLE_COLUMN_COUNT:
                 print(f"リストの件数が一致しないのでスキップします: csv {i+1} 行目")
                 continue
@@ -44,14 +54,32 @@ def main():
             id = list[0]
             url = list[1]
 
+            # SQL実行
             cursor.execute(f"INSERT INTO {TABLE_NAME} (id, url) VALUES ({id}, '{url}')")
-
             print(f"csv {i+1} 行目をテーブルにINSERTしました。")
-    
+
+
+def close_db(connect, cursor):
     cursor.close()
     connect.commit()
     connect.close()
-            
+
+
+def main():
+
+    config_read()
+
+    connect, cursor = connect_db()
+
+    # INSERT前にDROPする必要がなければコメントアウトしておく
+    drop_table(cursor)
+
+    create_table(cursor)
+
+    insert_db_from_csv(cursor)
+
+    close_db(connect, cursor)
+
 
 if __name__ == '__main__':
     main()
